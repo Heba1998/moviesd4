6791c318bb0989e522ef7a808f24d59c8e2f36a0
Heba1998 <>
2021-04-14 12:01:43 +0000

    lab

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Hello! You are in my project ^.^ ");
+
+var movies = prompt("Please enter the type of movies you want to watch ", "kids");
+
+if (movies== "kids" ) {
+ alert("I wish you an enjoyable watch ^.^ ");
+} else if (movies== "family" ) {
+   alert("I wish you an enjoyable watch ^.^ ");
+} else {
+   alert("I'm sorry, You will not find what you are looking for on my website ^_^ ");
+}
+
+document.write("I hope you enjoyed browsing my website ^.^");
