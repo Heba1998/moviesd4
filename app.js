b0f5659a1e3c59f3fd2c312d80656fc7890cfa32
Heba1998 <>
2021-04-18 11:06:13 +0000

    lab6

--- app.js
+++ app.js
@@ -3,23 +3,27 @@ alert("Hello! You are in my project ^.^ ");
 var movies = prompt("Please enter the type of movies you want to watch ", "kids");
 
 
-
-
-
-
 if (movies== "kids" ) {
 
-var moviesName = prompt('please enter your fav char name from kids movie!', 'minnie');
+
+var moviesName;
+var favKidmovies= function (){
+ moviesName = prompt('please enter your fav char name from kids movie!', 'minnie');
 
 while(moviesName !== 'minnie' && moviesName !== 'mickey')
 
 {
   moviesName = prompt('Please enter the name minnie or mickey');
 }
+}
+favKidmovies();
+
+
+
 
 
 var userChoose;
-
+var KidsMovImage= function(){
 if(moviesName == 'minnie'){
 userChoose='<img src="https://i.pinimg.com/originals/58/29/c3/5829c3e6e7437daac35272ab75cae2cc.png">';
 }
@@ -33,20 +37,38 @@ var imagesNumber= prompt('How many image do you want???');
 for(var i=0 ; i < imagesNumber; i++){
   document.write(userChoose);
 }
+}
+KidsMovImage();
+
 
  alert("I wish you an enjoyable watch ^.^ ");
 
-} else if (movies== "family" ) {
+} 
+
+
+
+
+
+else if (movies== "family" ) {
+
+  var moviesName;
+  var favFammovies= function (){
 
   var moviesName = prompt('please enter your fav char name from family movie!', 'Harry Potter');
 
 while(moviesName !== 'Harry Potter'&& moviesName !== 'Nemo'){
   moviesName = prompt('Please enter the name Harry Potter or Nemo');
 }
+  }
+
+favFammovies();  
+
+
+
 
 
 var userChoose;
-
+var FamilymoviesImg= function (){
 if(moviesName === 'Harry Potter'){
 userChoose='<img src="https://webbiesworld.com/wp-content/uploads/2020/10/Harry-Potter.jpg">';
 }else if (moviesName === 'Nemo'){
@@ -59,13 +81,18 @@ var imagesNumber= prompt('How many image do you want???');
 for(var i=0 ; i < imagesNumber; i++){
   document.write(userChoose);
 }
+}
+FamilymoviesImg();
   
    alert("I wish you an enjoyable watch ^.^ ");
 
 
 
 
-} else {
+}
+
+
+ else {
    alert("I'm sorry, You will not find what you are looking for on my website ^_^ ");
 }
 
